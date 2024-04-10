ec078493918f7cb35cd0101e31a478c17b06bbed
Ethan Goh <ethangoh98@gmail.com>
2024-04-10 10:18:06 +1000

    Adding some Icon support classes

--- src/components/CardSelect/AllCards.js
+++ src/components/CardSelect/AllCards.js
@@ -8,11 +8,18 @@ export default function AllCards(props){
     
     //Get all cards and display them in a grid.
     
-
+    const allCardsContainer = classNames("all_cards__container");
+    const allCardsColumn = classNames("all_cards__all_columns", {"all_cards__all_columns__selected": isSelected});
+    const expandedCardView = classNames("all_cards__singular_card_view", {"all_cards__singular_card_view__selected": isSelected})
 
     return (
-        <div ref={innerRef}>
-            
+        <div ref={innerRef} className={allCardsContainer}> 
+            <div className={allCardsColumn}>
+                
+            </div>
+            <div className={expandedCardView}>
+                
+            </div>
         </div>
     );
 }
