f9f35ce3089e389683a3c5ea47b353006f420786
Ethan Goh <ethangoh98@gmail.com>
2025-03-17 11:45:44 +1100

    move to a more logical ordering of how it should show on the FE

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -8,24 +8,26 @@ export default function ProjectCarousel(props){
     // List 5 items in a sort of stack, in actuality 7 but two are hidden.
     // try render the first three stacked ontop of each other
 
+    // 
+
     return(
         <div className='carousel_container'>
             <div className='carousel_container-centering'>
-
-                <div className="carousel_front-container">
-                    <img className="carousel_front-container_img" src={items[0].imgs[1]}></img>
+                
+                <div className='carousel_hidden-layer-left-container'>
+                    <img className="carousel_hidden-layer-left-container_img" src={items[0].imgs[2]}></img>
                 </div>
 
                 <div className='carousel_second-layer-left-container'>
                     <img className="carousel_second-layer-left-container_img" src={items[0].imgs[0]}></img>
                 </div>
 
-                <div className='carousel_second-layer-right-container'>
-                    <img className="carousel_second-layer-right-container_img" src={items[0].imgs[2]}></img>
+                <div className="carousel_front-container">
+                    <img className="carousel_front-container_img" src={items[0].imgs[1]}></img>
                 </div>
 
-                <div className='carousel_hidden-layer-left-container'>
-                    <img className="carousel_hidden-layer-left-container_img" src={items[0].imgs[2]}></img>
+                <div className='carousel_second-layer-right-container'>
+                    <img className="carousel_second-layer-right-container_img" src={items[0].imgs[2]}></img>
                 </div>
 
                 <div className='carousel_hidden-layer-right-container'>
