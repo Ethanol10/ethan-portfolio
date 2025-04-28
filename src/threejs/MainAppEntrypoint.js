3deca023c1a67f24b3b2e702b1d3eb1c8ee6be1d
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2025-04-28 21:46:51 +1000

    enable shadows

--- src/threejs/MainAppEntrypoint.js
+++ src/threejs/MainAppEntrypoint.js
@@ -9,6 +9,7 @@ export default class Main{
       this.scene = new THREE.Scene();
       this.scene_cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       this.renderer = new THREE.WebGLRenderer();
+      this.renderer.shadowMap.enabled = true;
       this.renderer.setSize(window.innerWidth, window.innerHeight);
 
       //Attach to dom element
@@ -18,36 +19,42 @@ export default class Main{
       let geometry = new THREE.BoxGeometry(1, 1, 1);
       let material = new THREE.MeshStandardMaterial({ color: 0xFF0000 });
       this.cube = new THREE.Mesh(geometry, material);
+      this.cube.castShadow = true;
+      this.cube.receiveShadow = true;
       this.scene.add(this.cube);
       this.cube.position.set(0,0,0);
 
       //Setup ground
       let groundGeometry = new THREE.PlaneGeometry(1, 1, 1);
-      let groundMaterial = new THREE.MeshStandardMaterial({color: 0x00FF00});
+      let groundMaterial = new THREE.MeshStandardMaterial({color: 0xd1ffbd});
       this.ground = new THREE.Mesh(groundGeometry, groundMaterial);
+      this.ground.receiveShadow = true;
       this.scene.add(this.ground);
       this.ground.position.set(0, -2, 0);
-      this.ground.scale.set(100, 100, 100);
+      this.ground.scale.set(30, 30, 30);
       this.ground.rotation.x = EulerToRad(-90);
 
       //Setup Ambient Light
-      this.ambientLight = new THREE.AmbientLight( 0x404040 );
+      this.ambientLight = new THREE.AmbientLight( 0xFFFFFF );
+      this.ambientLight.castShadow = true;
       this.scene.add(this.ambientLight);
 
       //Setup Point Light
       this.pointLight = new THREE.PointLight(0xFFFFFF, 50, 0);
       this.pointLight.position.set(0, 5, 0);
+      this.pointLight.castShadow = true;
       this.scene.add(this.pointLight)
 
       // Setup Directional Light
-      // const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-      // directionalLight.position.set(-1, 2, 4);
-      // scene.add(directionalLight);
+      // const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+      // directionalLight.position.set(0, 2, 4);
+      // directionalLight.castShadow = true;
+      // this.scene.add(directionalLight);
 
       //Setup camera
-      this.scene_cam.position.z = 3;
+      this.scene_cam.position.z = 5;
       this.scene_cam.position.y = 5;
-      this.scene_cam.rotation.x = EulerToRad(-65);
+      this.scene_cam.rotation.x = EulerToRad(-45);
 
       this.clock = new THREE.Clock();
 
