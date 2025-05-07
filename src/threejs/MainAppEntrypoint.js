66725489a5000f950d0812f674b3d92adabbd72c
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2025-05-08 03:30:51 +1000

    my god it's ugly

--- src/threejs/MainAppEntrypoint.js
+++ src/threejs/MainAppEntrypoint.js
@@ -4,10 +4,16 @@ import Boid from './objects/boid';
 import Ground from './objects/ground';
 import { TARGET_RENDER_FRAMETIME } from './StaticValues';
 
+export function getScene(){
+	return scene;
+}
+
+export let scene = null;
+
 export default class Main{
   //Assume center is calculated from (0,0) to (bounds, bounds)
     constructor(refContainer, bounds){
-      this.scene = new THREE.Scene();
+      scene = new THREE.Scene();
       this.scene_cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       this.renderer = new THREE.WebGLRenderer();
       this.renderer.shadowMap.enabled = true;
@@ -19,25 +25,23 @@ export default class Main{
       refContainer.current && refContainer.current.appendChild( this.renderer.domElement );
       
       this.ground = new Ground(this.clock);
-      this.scene.add(this.ground.getObj());
+      scene.add(this.ground.getObj());
 
       let centerPoint = this.ground.getCenterPoint();
 
       this.boid = new Boid(this.clock);
-      this.scene.add(this.boid.getObj());
       this.boid2 = new Boid(this.clock);
-      this.scene.add(this.boid2.getObj());
-      this.boid2.getObj().position.set(20, 0, 15);
+      // this.boid2.getObj().position.set(20, 0, 15);
 
       //Setup Ambient Light
       this.ambientLight = new THREE.AmbientLight( 0xFFFFFF );
-      this.scene.add(this.ambientLight);
+      scene.add(this.ambientLight);
 
       //Setup Point Light
       this.pointLight = new THREE.PointLight(0xFFFFFF, 50, 0);
       this.pointLight.position.set(centerPoint.x, 5, centerPoint.y);
       this.pointLight.castShadow = true;
-      this.scene.add(this.pointLight)
+      scene.add(this.pointLight)
 
       // Setup Directional Light
       // const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
@@ -61,7 +65,7 @@ export default class Main{
     }    
 
     Update(delta){
-      this.renderer.render(this.scene, this.scene_cam);
+      this.renderer.render(scene, this.scene_cam);
     }
 
     FixedUpdate(delta){
--- src/threejs/objects/boid.js
+++ src/threejs/objects/boid.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import { EulerToRad } from '../ThreeJSHelpers';
 import { BOID_BOUNDS } from '../StaticValues';
+import { GLTFLoader, OBJLoader } from "three/examples/jsm/Addons.js";
+import { getScene } from '../MainAppEntrypoint';
+import airplane from '../../media/models/airplane.glb';
 
 export default class Boid{
     constructor(clock){
@@ -9,17 +12,37 @@ export default class Boid{
         this.AMPLITUDE = 1;
         this.SPEED = 4;
 
-        console.log("selectedWavelength: ", this.WAVELENGTH);
+        new GLTFLoader().load(airplane, (obj) => {this.onLoad(obj)}, this.onLoading, this.onLoadError);
+        // let objLoader = new OBJLoader();
 
         this.clock = clock;
-        let geometry = new THREE.BoxGeometry(1, 1, 1);
+        
+        this.directionalVector = new THREE.Vector3(1,0,0); 
+        console.log("load lol");
+    }
+
+    onLoad(gltf){
+        //Setup the object.
+        this.obj = gltf.scene;
         let material = new THREE.MeshStandardMaterial({ color: 0x0000FF });
-        this.obj = new THREE.Mesh(geometry, material);
+        this.obj.material = material;
+
+        // this.obj = new THREE.Mesh(mesh, material);
         this.obj.castShadow = true;
         this.obj.receiveShadow = true;
         this.obj.position.set(BOID_BOUNDS / 2, 0, BOID_BOUNDS / 2);
+        this.obj.scale.set(2,2,2);
         
-        this.directionalVector = new THREE.Vector3(1,0,0); 
+        //add to Scene
+        getScene().add(this.obj);
+    }
+
+    onLoading(xhr){
+        console.log( (xhr.loaded / (xhr.total * 100)) + "% loaded");
+    }
+
+    onLoadError(error){
+        console.log(error);
     }
 
     getObj(){
@@ -27,15 +50,25 @@ export default class Boid{
     }
 
     Update(delta){
+        if(!this.obj){
+            return;
+        }
+
         this.obj.rotation.x += EulerToRad(90) * delta;
-        this.obj.rotation.y += EulerToRad(90) * delta;
+        // this.obj.rotation.y += EulerToRad(90) * delta;
     
         //Bob up and down
-        // this.obj.position.y = this.HOVER_POSITION + (this.AMPLITUDE * Math.sin(this.WAVELENGTH * this.clock.elapsedTime));
-        this.Move(delta);
+        this.obj.position.y = this.HOVER_POSITION + (this.AMPLITUDE * Math.sin(this.WAVELENGTH * this.clock.elapsedTime));
+
+
+        // this.Move(delta);
     }
 
     Move(delta){
+        if(!this.obj){
+            return;
+        }
+
         this.obj.position.x += this.directionalVector.x * this.SPEED * delta;
         this.obj.position.z += this.directionalVector.z * this.SPEED * delta;
 
