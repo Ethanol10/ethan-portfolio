aa8b0a63b1c58a545c15c3046ba1f157872437b7
Ethan Goh <ethangoh98@gmail.com>
2025-08-04 19:23:17 +1000

    some vertex modification testing

--- src/pages/portfolio/threejs/objects/ground.js
+++ src/pages/portfolio/threejs/objects/ground.js
@@ -1,25 +1,45 @@
 import * as THREE from 'three';
 import { EulerToRad } from '../ThreeJSHelpers';
 import { BOID_BOUNDS } from '../StaticValues';
-import { getScene } from '../MainAppEntrypoint';
+import { getScene, getClock, AddNewObject } from '../MainAppEntrypoint';
 import { generatePerlinNoise } from '../ThreeJSHelpers';
 // import { fragShader } from '../shader/groundshaderfrag';
-import { vertexShader } from '../shader/groundshadervert'; 
+// import { vertexShader } from '../shader/groundshadervert'; 
 
 export default class Ground{
     constructor(){
-        let groundGeometry = new THREE.PlaneGeometry(1, 1, 100, 100);
+        let groundGeometry = new THREE.PlaneGeometry(1, 1, 1000, 1000);
+        // groundGeometry.rotateX(EulerToRad(-90));
         this.groundMaterial = new THREE.MeshStandardMaterial({color: 0xd1ffbd});
         this.perlinTex = generatePerlinNoise(256, 256);
+        this.clock = getClock();
         //material modification
-        // this.groundMaterial.onBeforeCompile = (shader) => {
-        //     shader.vertexShader = shader.vertexShader.replace(
-        //         "#include <begin_vertex>", 
-        //         `
-        //             vec3 transformed = position + normal * sin(position.y * 10.0) * 0.1;
-        //         `
-        //     )
-        // }
+        this.groundMaterial.onBeforeCompile = (shader) => {
+            shader.vertexShader = `
+                uniform float time;
+            ` + shader.vertexShader;
+
+            // shader.vertexShader = shader.vertexShader.replace(
+            //     "#include <begin_vertex>", 
+            //     `
+            //         // BEGIN custom pillar displacement
+            //         vec3 transformed = vec3(position);
+
+            //         float blockSize = 1.0;
+            //         float bx = floor(transformed.x / blockSize);
+            //         float bz = floor(transformed.z / blockSize);
+
+            //         // Simple pillar height pattern
+            //         float pillarHeight = mod(bx + bz, 2.0) * 2.0;
+
+            //         // Animate height
+            //         pillarHeight *= sin(time + bx * 0.5 + bz * 0.5);
+
+            //         transformed.y += pillarHeight;
+            //         // END custom pillar displacement
+            //     `
+            // )
+        }
         
         this.ground = new THREE.Mesh(groundGeometry, this.groundMaterial);
         this.ground.receiveShadow = true;
@@ -48,6 +68,7 @@ export default class Ground{
         */
 
         getScene().add(this.ground);
+        AddNewObject(this);
     }
 
     getBounds(){
@@ -68,7 +89,15 @@ export default class Ground{
         }
     }
 
+    FixedUpdate(delta){
+        if(this.clock){
+            this.groundMaterial.uniforms = {
+                time: { value: this.clock.getElapsedTime() }
+            };
+        }
+    }
+
     Update(delta){
-        
+
     }
 }
